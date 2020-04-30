958bf9ccda6f3eb1aa17d6da6f3a53151142a19a
xsway <kgulordava@gmail.com>
2020-04-30 10:23:25 +0200

    copy to clipboard stump; toast copied

--- src/components/manage_participants/Status.js
+++ src/components/manage_participants/Status.js
@@ -1,12 +1,22 @@
 
 import React from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Dropdown, Button, ButtonGroup } from 'react-bootstrap'
+import { Dropdown, Button, ButtonGroup, Toast } from 'react-bootstrap'
 
 export const ParticipantStatus = ({
     value,
 }) => {
     const { t } = useTranslation();
+
+    const [show, setShow] = useState(false);
+
+    const copyToClipboard = (e) => {
+        var dummyContent = "this is to be copied to clipboard";
+        // var dummy = $('<input>').val(dummyContent).appendTo('body').select()
+        document.execCommand('copy')
+        setShow(true)
+    }
 
     switch (value) {
 
@@ -17,16 +27,21 @@ export const ParticipantStatus = ({
 
         case 'MUST_SEND_EMAIL':
             return (
+
                 <Dropdown as={ButtonGroup}>
                     <Button variant="warning" size="sm">{t('manageParticipants.sendBCForm')}</Button>
-
                     <Dropdown.Toggle split variant="warning btn-sm" id="dropdown-split-basic" size="sm" />
-
+                    <Toast style={{
+                        position: 'absolute',
+                        top: 0,
+                        right: -80,
+                    }} onClose={() => setShow(false)} show={show} delay={3000} animation={true} autohide><Toast.Body className="badge">Copied!</Toast.Body></Toast>
                     <Dropdown.Menu>
                         <Dropdown.Item href="#/action-1">{t('manageParticipants.sendByEmail')}</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">{t('manageParticipants.sendByLink')}</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2" onClick={copyToClipboard}>{t('manageParticipants.sendByLink')}</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
+
             );
 
         case 'EMAIL_SENT':

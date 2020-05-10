25d05fe23fbefd76b293a46adfdfabc0303ab684
xsway <kgulordava@gmail.com>
2020-05-10 20:48:04 +0200

    rounded badges participants

--- src/pages/Simulation/components/ParticipantsTable.js
+++ src/pages/Simulation/components/ParticipantsTable.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
+import { ParticipantItemForm } from '../../Participants/components/ParticipantItemForm';
+import styled from 'styled-components';
+import { COLORS } from '../../../vars';
 
 const ParticipantsTable = ({
   workshopParticipants,
@@ -8,34 +11,51 @@ const ParticipantsTable = ({
   handleSelect,
   t,
 }) => {
+  const ParticipantItemBadge = ({
+    id,
+    firstName,
+    hearts,
+    selected,
+  }) => {
+    console.log("Select button", selected)
+    
+    return <StyledItem
+        name={id}
+        value={id}
+        className="m-1 pl-3 pr-3 p-1 btn-block rounded-pill"
+        selected={selected}
+        onClick={(e) => {handleSelect(id)}}>
+        {firstName}<span className="float-right">{hearts} &#x2764;</span>
+        </StyledItem>           
+  }
+
   return (
-    <Table borderless striped hover>
-      <tbody>
+    <div>
         {workshopParticipants &&
           entityParticipants &&
           workshopParticipants.map((participantId) => {
             const { email, firstName, lastName } = entityParticipants[
               participantId
             ];
-            const style =
-              selectedParticipantId === participantId
-                ? {
-                    backgroundColor: 'gray',
-                  }
-                : {};
             return (
-              <tr
-                key={email}
-                onClick={() => handleSelect(participantId)}
-                style={style}
-              >
-                <td>{firstName}</td>
-                <td>{lastName}</td>
-              </tr>
+              <ParticipantItemBadge 
+                id={participantId}
+                firstName={firstName}
+                hearts={5}
+                selected={selectedParticipantId === participantId}                
+              />
             );
           })}
-      </tbody>
-    </Table>
+    </div>
   );
 };
+
+const StyledItem = styled.div`
+cursor: pointer;
+color: white;
+//   border: ${props => props.selected ? '3pt solid palegreen' : '3pt solid white' };
+background: ${props => props.selected ? COLORS.BLUE.STANDARD : COLORS.GRAY.STANDARD};
+`;
+
+
 export default ParticipantsTable;

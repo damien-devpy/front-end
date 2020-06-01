b863698dd048c206b1808366e91198dc45f9953d
Xavier Arques <xavarques@gmail.com>
2020-06-01 16:54:10 +0200

    Updates

---
+++ precache-manifest.ccf53f1908ad7a828ced79c1c8bc925d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060d85c2c07bd1824381b3ec2065c58f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00c8647b998c89baf2b1",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5610bf6c6c6b2d324699",
+    "url": "/static/css/main.11a5fa7c.chunk.css"
+  },
+  {
+    "revision": "00c8647b998c89baf2b1",
+    "url": "/static/js/2.81e73498.chunk.js"
+  },
+  {
+    "revision": "26276ad57cd197a7db42445302e8a7d6",
+    "url": "/static/js/2.81e73498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5610bf6c6c6b2d324699",
+    "url": "/static/js/main.7f69516f.chunk.js"
+  },
+  {
+    "revision": "10e18084de1363b5c050",
+    "url": "/static/js/runtime-main.7cae99bc.js"
+  },
+  {
+    "revision": "77705799ab05db42974183e59c326fe3",
+    "url": "/static/media/NotoEmoji-Regular.77705799.ttf"
+  },
+  {
+    "revision": "bd95c919d5566390c0565bea43f95495",
+    "url": "/static/media/Symbola-AjYx.bd95c919.ttf"
+  }
+]);

891d7f6f6f2031d1a912938b13db3951283b163f
Noé Keren <noe.keren@learnenjoy.com>
2020-06-04 14:53:13 +0200

    Revert "en cours"
    This reverts commit e2e10927b359dd67b1fde7d4a81b6e69ca324768.

--- src/utils/mocks/participants.js
+++ src/utils/mocks/participants.js
@@ -1,43 +1,43 @@
-// // import { MUST_SEND_EMAIL, BILAN_RECEIVED, EMAIL_SENT } from '../../reducers/participants';
+import { MUST_SEND_EMAIL, BILAN_RECEIVED, EMAIL_SENT } from '../../reducers/participants';
 
-// export default {
-//   byId: {
-//     1: {
-//       firstName: 'François',
-//       lastName: 'Laugier',
-//       email: 'francois_laugier@outlook.com',
-//       status: MUST_SEND_EMAIL,
-//       isValid: true,
-//       linkBC: null,
-//       personaId: null,
-//     },
-//     2: {
-//       firstName: 'Xavier',
-//       lastName: 'Arques',
-//       email: 'xavarques@gmail.com',
-//       status: BILAN_RECEIVED,
-//       isValid: true,
-//       linkBC: 'http//www',
-//       personaId: null,
-//     },
-//     3: {
-//       firstName: 'Ivan',
-//       lastName: 'Dorne',
-//       email: 'ivan.dorne@gmail.com',
-//       status: EMAIL_SENT,
-//       isValid: true,
-//       linkBC: 'http//www',
-//       personaId: null,
-//     },
-//     4: {
-//       firstName: 'Yvan',
-//       lastName: 'Dornet',
-//       email: 'ivan.dornet@gmail.com',
-//       status: BILAN_RECEIVED,
-//       isValid: true,
-//       linkBC: null,
-//       personaId: 2,
-//     },
-//   },
-//   allIds: [1, 2, 3, 4],
-// };
+export default {
+  byId: {
+    1: {
+      firstName: 'François',
+      lastName: 'Laugier',
+      email: 'francois_laugier@outlook.com',
+      status: MUST_SEND_EMAIL,
+      isValid: true,
+      linkBC: null,
+      personaId: null,
+    },
+    2: {
+      firstName: 'Xavier',
+      lastName: 'Arques',
+      email: 'xavarques@gmail.com',
+      status: BILAN_RECEIVED,
+      isValid: true,
+      linkBC: 'http//www',
+      personaId: null,
+    },
+    3: {
+      firstName: 'Ivan',
+      lastName: 'Dorne',
+      email: 'ivan.dorne@gmail.com',
+      status: EMAIL_SENT,
+      isValid: true,
+      linkBC: 'http//www',
+      personaId: null,
+    },
+    4: {
+      firstName: 'Yvan',
+      lastName: 'Dornet',
+      email: 'ivan.dornet@gmail.com',
+      status: BILAN_RECEIVED,
+      isValid: true,
+      linkBC: null,
+      personaId: 2,
+    },
+  },
+  allIds: [1, 2, 3, 4],
+};

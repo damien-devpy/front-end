77034a9e57d9d9e4997c58ddb6e9538f873af0ef
xsway <kgulordava@gmail.com>
2020-06-06 19:42:26 +0200

    remove mock participants

--- src/utils/mock.js
+++ src/utils/mock.js
@@ -1,5 +1,4 @@
 import coaches from './mocks/coaches';
-import participants from './mocks/participants';
 import personas from './mocks/personas';
 import workshop from './mocks/workshop';
 import workshops from './mocks/workshops';
@@ -9,6 +8,5 @@ export default {
   [`/coaches`]: coaches,
   [`/workshop`]: workshop,
   [`/workshops`]: workshops,
-  [`/participants`]: participants,
   [`/personas`]: personas,
 };

53861a7ddde4385d7187d3613ee092cee1f13ff9
xsway <kgulordava@gmail.com>
2020-07-15 17:12:47 +0200

    card header component

---
+++ src/components/CardHeader.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  // margin-bottom: 1rem;
+`;
+
+export default StyledHeader;

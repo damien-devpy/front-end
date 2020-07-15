9395952aaffa556fc3be6726b249c167bb822aa5
xsway <kgulordava@gmail.com>
2020-07-15 17:17:39 +0200

    small margin updates

--- src/pages/Participants/components/NewParticipantItem.js
+++ src/pages/Participants/components/NewParticipantItem.js
@@ -50,7 +50,7 @@ export const ParticipantItemForm = ({
     });
 
     return (
-      <Form.Control
+      <Form.Control custom
         as="select"
         size="sm"
         id="dropdown"
@@ -122,8 +122,8 @@ export const ParticipantsHeader = () => {
 const StyledHeaderRow = styled.div`
   //background-color: ${COLORS.GRAY.LIGHT};
   margin-top: 10px;
-  margin-bottom:30px;
+  // margin-bottom:30px;
   padding-bottom: 5px;
   font-weight: bold;
-  border-bottom:1px solid #E2E0E0;
+  // border-bottom:1px solid #E2E0E0;
 `;

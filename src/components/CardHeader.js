817b4e111c22f48b272eb513bb94cd9d0d79955e
xsway <kgulordava@gmail.com>
2020-07-17 14:05:26 +0200

    card header remove height stretch

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const StyledHeader = styled.div`
   display: flex;
+  align-items: center;
   justify-content: space-between;
-  // margin-bottom: 1rem;
 `;
 
 export default StyledHeader;

732b7e375703c260ac9f8d04d59fdb0e5b320af3
xsway <kgulordava@gmail.com>
2020-07-25 14:48:18 +0200

    eslint clean

--- src/components/CommonModal.js
+++ src/components/CommonModal.js
@@ -19,7 +19,10 @@ const CommonModal = ({
       show={show}
       onHide={handleClose}
     >
-      <Modal.Header closeButton={handleClose !== undefined} className="alert-primary">
+      <Modal.Header
+        closeButton={handleClose !== undefined}
+        className="alert-primary"
+      >
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>{children}</Modal.Body>

096c210ba65973b8607dc229e6dc7c896aee2a79
Xavier Arques <xavarques@gmail.com>
2020-08-02 23:22:18 +0200

    Manage password change

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,7 +1,12 @@
+import { changePassword } from '../utils/api';
+import { logout } from '../utils/auth';
+
 export const AUTHENTICATE_CURRENT_USER = 'AUTHENTICATE_CURRENT_USER';
 export const CURRENT_USER_RETRIEVED = 'CURRENT_USER_RETRIEVED';
 export const CURRENT_USER_LOAD_ERROR = 'CURRENT_USER_LOAD_ERROR';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const CHANGE_PASSWORD_CURRENT_USER = 'CHANGE_PASSWORD_CURRENT_USER';
+export const PASSWORD_CHANGED = 'PASSWORD_CHANGED';
 
 export const currentUserRetrieved = (user) => ({
   type: CURRENT_USER_RETRIEVED,
@@ -18,7 +23,30 @@ export const authenticateCurrentUser = () => ({
   payload: {},
 });
 
-export const logoutCurrentUser = () => ({
-  type: LOGOUT_CURRENT_USER,
+export const logoutCurrentUser = () => {
+  logout();
+  return {
+    type: LOGOUT_CURRENT_USER,
+    payload: {},
+  };
+};
+
+const requestChangePassword = () => ({
+  type: CHANGE_PASSWORD_CURRENT_USER,
   payload: {},
 });
+
+const passwordChanged = () => ({
+  type: PASSWORD_CHANGED,
+  payload: {},
+});
+
+export const changeCurrentUserPassword = (password) => {
+  return (dispatch) => {
+    dispatch(requestChangePassword());
+    return changePassword({ password }).then(() => {
+      dispatch(logoutCurrentUser());
+      dispatch(passwordChanged());
+    });
+  };
+};
